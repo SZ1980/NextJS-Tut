53506b75750870b38e7f6a311486fee3fe389deb
Zoran Simonovic <simonovic1980@gmail.com>
2023-03-29 22:31:38 +0200

    adding commit to second part

---
+++ next-01/app/about/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+
+function About() {
+
+    // throw new Error('Not Today!')
+  return (
+    <>
+    <h1>About</h1>
+    <Link href='/'>Link To Home Page</Link>
+    </>
+  )
+}
+
+export default About
